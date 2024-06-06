1dcf7e56582229c61c6ba7f7b9b162f0a1059d19
abh1nav <abhinavsharma00410@gmail.com>
2024-06-06 16:42:47 +0530

    DAY1

---
+++ first.cpp
@@ -0,0 +1,31 @@
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// int main(){
+//     int n;
+//     cin>>n;
+//     if(n>18){
+//         cout<<"Eligible to code";
+//     }
+//     else{
+//         cout<<"Not Eligible to code";
+//     }
+// }
+
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int a,b,c;
+    cin>>a>>b>>c;
+    if(a>b && a>c){
+        cout<<a<<" is greatest";
+    }
+    else if(b>a && b>c){
+        cout<<b<<" is greatest";
+        
+    }
+    else{
+        cout<<c<<" is greatest";
+
+    }
+}
