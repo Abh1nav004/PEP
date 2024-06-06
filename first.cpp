b79e49d2d779d1fd8df0baa9b38bf5bfb5ad688e
abh1nav <abhinavsharma00410@gmail.com>
2024-06-06 16:50:02 +0530

    change 1 of day1

--- first.cpp
+++ first.cpp
@@ -12,20 +12,33 @@
 //     }
 // }
 
+// #include <bits/stdc++.h>
+// using namespace std;
+// int main(){
+//     int a,b,c;
+//     cin>>a>>b>>c;
+//     if(a>b && a>c){
+//         cout<<a<<" is greatest";
+//     }
+//     else if(b>a && b>c){
+//         cout<<b<<" is greatest";
+        
+//     }
+//     else{
+//         cout<<c<<" is greatest";
+
+//     }
+// }
+
+
 #include <bits/stdc++.h>
 using namespace std;
 int main(){
-    int a,b,c;
-    cin>>a>>b>>c;
-    if(a>b && a>c){
-        cout<<a<<" is greatest";
-    }
-    else if(b>a && b>c){
-        cout<<b<<" is greatest";
-        
-    }
-    else{
-        cout<<c<<" is greatest";
-
+    int n;
+    cin>>n;
+    int i=1;
+    while(i<=n){
+        cout<<i<<" ";
+        i++;
     }
 }
