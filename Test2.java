e7cb0d48ee497da2668c49bcd549c05333342acc
abh1nav <abhinavsharma00410@gmail.com>
2024-06-07 15:48:53 +0530

    DAY 2 commit 2

--- Test2.java
+++ Test2.java
@@ -1,28 +1,52 @@
-import java.util.*;
-public class Test2{
-    public static void main(String[] args) {
-        int marks[]=new int[5];
-        // int numbers[] = {1,2,3};
-        // int moreNumbers[] = {4,5,6};
-        // String fruits[] = {"apple","mango","orange"};
+// import java.util.*;
+// public class Test2{
+//     public static void main(String[] args) {
+//         int marks[]=new int[5];
+//         // int numbers[] = {1,2,3};
+//         // int moreNumbers[] = {4,5,6};
+//         // String fruits[] = {"apple","mango","orange"};
 
-        // System.out.println(marks[0]);
-        // System.out.println(numbers[0]);
-        // System.out.println(moreNumbers[0]);
-        // System.out.println(fruits[0]);
+//         // System.out.println(marks[0]);
+//         // System.out.println(numbers[0]);
+//         // System.out.println(moreNumbers[0]);
+//         // System.out.println(fruits[0]);
 
-        Scanner sc = new Scanner(System.in);
-        for(int i=0;i<marks.length;i++){
+//         Scanner sc = new Scanner(System.in);
+//         for(int i=0;i<marks.length;i++){
 
-            marks[i]=sc.nextInt();
-        }
+//             marks[i]=sc.nextInt();
+//         }
         
-        for(int i=0;i<marks.length;i++){
+//         for(int i=0;i<marks.length;i++){
             
-            System.out.print(marks[i]+" ");
+//             System.out.print(marks[i]+" ");
+//         }
+        
+//         sc.close();
+//     }
+// }
+
+
+import java.util.*;
+public class Test2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = scanner.nextInt();
+        int arr[] = new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=scanner.nextInt();
         }
-        
-        
-        sc.close();
-    }
+        int key = scanner.nextInt();
+        for(int i=0;i<n;i++){
+            if(key==arr[i]){
+                System.out.println("Key found at index: "+i);
+                break;
+            }
+            else{
+                System.out.println("Key not found!");
+            }
+        }
+        scanner.close();
+    } 
 }
